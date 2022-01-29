e675f35120e6c9f90ca6af7aa548be35b3a40933
preegnees <mrraziapov13@gmail.com>
2022-01-29 21:51:51 +0300

    убрал ненужные импорты

--- src/main/koltin/ru/radmir/synServer/synServer/SynServerApplication.java
+++ src/main/koltin/ru/radmir/synServer/synServer/SynServerApplication.java
@@ -1,12 +1,7 @@
 package ru.radmir.synServer.synServer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.radmir.synServer.synServer.init.Init;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SynServerApplication {
